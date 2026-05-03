84e82e10dd88e3a218e2ca39d516b8f8e5bc0a44
Luis Fellipe <luisfellipericardolisboa@gmail.com>
2026-05-03 00:20:30 -0300

    Correção da exibição da possibilidade de chuva

--- src/layout/Display.tsx
+++ src/layout/Display.tsx
@@ -4,21 +4,19 @@ import Filter from '../components/Filter'
 import styles from './Display.module.css'
 
 export default function Display(){
-    const date = new Date()
     const [data, setData] = useState<any>(null)
     const [key, setKey] = useState<number>(0)
 
     function getKey(data: any){
+        const date = new Date()
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
         const hours = String(date.getHours()).padStart(2, '0');
         const validateDate = `${year}-${month}-${day}T${hours}:00`;
         for(let i = 0; i < data?.hourly?.time?.length; i++){
-            console.log(data?.hourly?.time?.[i], validateDate)
             if(data?.hourly?.time?.[i] === validateDate){
                 setKey(i)
-                console.log(i)
                 break
             }
         }
@@ -26,6 +24,11 @@ export default function Display(){
 
     useEffect(() => {
         getKey(data)
+        const timer = setInterval(() => {
+            getKey(data);
+        }, 60000);
+
+        return () => clearInterval(timer);
     },[data])
 
     return(
